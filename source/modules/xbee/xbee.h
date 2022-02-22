8a9700c532f2e85232bffd1107c0fa6cac0161a6
WillMerges <wdmerges@gmail.com>
2022-02-22 13:07:08 -0500

    move xbee lib to correct dir

--- source/modules/xbee/xbee.h
+++ source/modules/xbee/xbee.h
@@ -12,12 +12,17 @@
 
 #define XBEE_BROADCAST_ADDR 0x000000000000FFFF
 
+typedef enum {
+    XB_OK,
+    XB_ERR
+} xb_ret_t;
+
 // init function
 // 'write' is a function that the API will use to output data to the XBee
 // NOTE: places the XBee in API mode, 
 //       this function must be called within 1s of power up or the XBee 
 //       cannot be placed in command mode
-void xb_init(void (*write) (uint8_t* buff, size_t len));
+xb_ret_t xb_init(int (*write) (uint8_t* buff, size_t len));
 
 // function that should be called when any data is received from the XBee (either over serial or SPI)
 // needs to be called by lower layer
@@ -32,7 +37,7 @@ void xb_attach_rx_callback(void (*rx) (uint8_t* buff, size_t len));
 void xb_set_dst(uint64_t addr);
 
 // transmit data
-void xb_tx(uint8_t* data, size_t len);
+xb_ret_t xb_tx(uint8_t* data, size_t len);
 
 typedef enum {
     XBEE_DIO0,
@@ -46,6 +51,6 @@ typedef enum {
 } xbee_dio_output_t;
 
 // remotely command digital I/O
-void xb_cmd_dio(xbee_dio_t dio, xbee_dio_output_t output);
+xb_ret_t xb_cmd_dio(xbee_dio_t dio, xbee_dio_output_t output);
 
 #endif
