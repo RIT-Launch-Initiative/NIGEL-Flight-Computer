6c8f912c3fbd5e91d668ed13716de0a5f4f46ba3
Aaron <fireninja108@gmail.com>
2022-02-23 19:05:19 -0500

    raw_rcv changes to struct

--- source/modules/xbee/xbee.c
+++ source/modules/xbee/xbee.c
@@ -25,19 +25,17 @@ typedef struct {
     uint16_t length;
     uint8_t frame_type;
     uint64_t src_address_64;
-    uint16_t src_address_16;
-    uint8_t options;
-    uint64_t received;
-    uint8_t checksum;
+    uint16_t reserved;
 } xb_rx_frame_t;
 
 static uint8_t tx_buff[1024];
+static uint8_t rx_buff[1024];
 
 xb_ret_t xb_tx(uint8_t *data, size_t len) {
     xb_tx_frame_t *frame = (xb_tx_frame_t *) tx_buff;
 
     frame->start_delimiter = 0x7E;
-    frame->length = 0x05;
+    frame->length = len;
     frame->frame_type = 0x10;
     frame->dst_address_64 = 0x000000000000FFFF;
     frame->reserved = 0xFFFE;
@@ -53,6 +51,7 @@ xb_ret_t xb_tx(uint8_t *data, size_t len) {
     }
 
     check = 0xff - check;
+    memcpy(tx_buff + frame->length + sizeof(xb_tx_frame_t), &check, 1);
 
     return XB_OK;
 }
@@ -64,17 +63,14 @@ void xb_attach_rx_callback(void (*rx)(uint8_t *buff, size_t len)) {
 
 
 void xb_raw_recv(uint8_t *buff, size_t len) {
-    xb_rx_frame_t *frame = (xb_rx_frame_t *) tx_buff;
+    xb_rx_frame_t *frame = (xb_rx_frame_t *) rx_buff;
 
-    // TODO: Figure out what options need to be set to
-    frame->start_delimiter = 0x90;
-    frame->length = 0x05;
+    frame->start_delimiter = 0x7E;
+    frame->length = len;
     frame->frame_type = 0x90;
-    frame->src_address_64 = 0x000000000000FFFF; // ?
-    frame->src_address_16 = 0xFFFE; // ?
-    frame->options = 0x02; // ?
-    frame->received = 0x00; // ?
-    frame->checksum = 0xFF - (sizeof(frame) - sizeof(frame->length) - sizeof(frame->start_delimiter));
+    frame->src_address_64 = 0x000000000000FFFF;
+    frame->reserved = 0xFFFE;
+
 
 }
 
@@ -85,6 +81,8 @@ void xb_set_dst(uint64_t addr) {
 
 xb_ret_t xb_cmd_dio(xbee_dio_t dio, xbee_dio_output_t output) {
 
+
+    return XB_OK;
 }
 
 
