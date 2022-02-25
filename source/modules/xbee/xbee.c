87029d5afa7a7e926a9a0abed3c79301c79a7efc
WillMerges <wdmerges@gmail.com>
2022-02-25 00:44:19 -0500

    refactor xbee at cmd send

--- source/modules/xbee/xbee.c
+++ source/modules/xbee/xbee.c
@@ -40,8 +40,6 @@ typedef struct {
     uint16_t reserved;
     uint8_t options;
     uint8_t at_command[2];
-    uint8_t param_value;
-    uint8_t checksum;
 } xb_at_frame_t;
 
 int (*xb_write)(uint8_t *buf, size_t len);
@@ -94,49 +92,62 @@ void xb_set_dst(uint64_t addr) {
     dst_addr = addr;
 }
 
-xb_ret_t xb_cmd_dio(xb_dio_t dio, xb_dio_output_t output) {
+static xb_ret_t xb_at_cmd(const char[2] cmd, const char* param) {
     xb_at_frame_t *frame = (xb_at_frame_t *) tx_buff;
 
+    size_t param_size = strlen(param);
+
     frame->header.start_delimiter = 0x7E;
-    // length = frame - header - checksum
-    frame->header.length = hton16(sizeof(xb_at_frame_t) - sizeof(xb_header_t) - 1);
+    // length = frame - header + param
+    frame->header.length = hton16(sizeof(xb_at_frame_t) - sizeof(xb_header_t) + param_size);
     frame->frame_type = 0x17;
     frame->frame_id = 0;
     frame->dst_address_64 = dst_addr;
     frame->reserved = hton16(0xFFFE);
     frame->options = 0x02; // apply changes immediately on remote
+    frame->at_command[0] = cmd[0];
+    frame->at_command[1] = cmd[1];
+
+    // copy parameter
+    memcpy(tx_buff + sizeof(xb_at_frame_t), param, param_size);
+
+    uint8_t check = 0;
+    size_t i;
+    for(i = sizeof(xb_header_t); i < sizeof(xb_at_frame_t) + param_size; i++) {
+        check += at_buff[i];
+    }
+
+    tx_buff[i] = 0xFF - check;
+
+    size_t len = sizeof(xb_at_frame_t) + param_size + 1;
+    if(xb_write(tx_buff, len) < len) {
+        // write error
+        return XB_ERR;
+    }
+
+    return XB_OK;
+}
+
+xb_ret_t xb_cmd_dio(xb_dio_t dio, xb_dio_output_t output) {
+    char cmd[2];
 
     switch(dio) {
         case XB_DIO12:
             // P2
-            frame->at_command[0] = 'P';
-            frame->at_command[1] = '2';
+            cmd[0] = 'P';
+            cmd[1] = '2';
             break;
         default:
             return XB_ERR;
     }
 
     // TODO I'm not sure if the parameter is passed as ASCII or binary
+    char* param = "4";
     if(output == XB_DIO_HIGH) {
-        frame->param_value = '5';
-    } else {
-        frame->param_value = '4';
-    }
+        param = "5";
+    } // else low (4)
 
-    uint8_t check = 0;
-    for(size_t i = sizeof(xb_header_t); i < sizeof(xb_at_frame_t) - 1; i++) {
-        check += at_buff[i];
-    }
-
-    check = 0xff - check;
-    frame->checksum = check;
-
-    if(xb_write(tx_buff, sizeof(xb_at_frame_t)) < sizeof(xb_at_frame_t)) {
-        // write error
-        return XB_ERR;
-    }
-
-    return XB_OK;
+    return xb_at_cmd(cmd, param);
 }
 
 xb_ret_t xb_init(int (*write)(uint8_t *buf, size_t len)) {
