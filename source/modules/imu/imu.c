855ad454fd02c9035fc930f318557626cce076bd
Aaron <fireninja108@gmail.com>
2022-03-01 14:37:02 -0500

    Formatting to IMU modules

--- source/modules/imu/imu.c
+++ source/modules/imu/imu.c
@@ -33,8 +33,8 @@
 #define PRESSURE_OSR_4096 0x48
 
 #define TEMP_OSR_256      0x50
-#define TEMP_OSR_512  	  0x52
-#define TEMP_OSR_1024 	  0x54
+#define TEMP_OSR_512      0x52
+#define TEMP_OSR_1024      0x54
 #define TEMP_OSR_2048     0x56
 #define TEMP_OSR_4096     0x58
 
@@ -74,6 +74,7 @@ static void ms5611_write(uint8_t data);
  * @return the value read on the SPI bus
  */
 static uint16_t ms5611_read16bits(uint8_t reg);
+
 static uint32_t ms5611_read24bits(uint8_t reg);
 
 /**
@@ -82,11 +83,11 @@ static uint32_t ms5611_read24bits(uint8_t reg);
  * @return the value read on the SPI bus
  */
 static uint32_t ms5611_readRawTemp();
+
 static uint32_t ms5611_readRawPressure();
 
 
-static void ms5611_init()
-{
+static void ms5611_init() {
     MS5611_DIS
     HAL_Delay(10);
 
@@ -101,15 +102,13 @@ static void ms5611_init()
     prom[5] = ms5611_read16bits(CMD_PROM_C6);
 }
 
-static void ms5611_write(uint8_t data)
-{
+static void ms5611_write(uint8_t data) {
     MS5611_EN
     HAL_SPI_Transmit(&hspi2, &data, 1, SPI_TIMEOUT);
     MS5611_DIS
 }
 
-static uint16_t ms5611_read16bits(uint8_t reg)
-{
+static uint16_t ms5611_read16bits(uint8_t reg) {
     uint8_t byte[3];
     uint16_t return_value;
     MS5611_EN
@@ -121,23 +120,21 @@ static uint16_t ms5611_read16bits(uint8_t reg)
      * actual data at that time (it was listening for command), data[0] will
      * contain garbage data (probably all 0's).
      */
-    return_value = ((uint16_t)byte[1]<<8) | (byte[2]);
+    return_value = ((uint16_t) byte[1] << 8) | (byte[2]);
     return return_value;
 }
 
-static uint32_t ms5611_read24bits(uint8_t reg)
-{
+static uint32_t ms5611_read24bits(uint8_t reg) {
     uint8_t byte[4];
     uint32_t return_value;
     MS5611_EN
     HAL_SPI_TransmitReceive(&hspi2, &reg, byte, 4, SPI_TIMEOUT);
     MS5611_DIS
-    return_value = ((uint32_t)byte[1]<<16) | ((uint32_t)(byte[2]<<8)) | (byte[3]);
+    return_value = ((uint32_t) byte[1] << 16) | ((uint32_t)(byte[2] << 8)) | (byte[3]);
     return return_value;
 }
 
-static uint32_t ms5611_readRawTemp()
-{
+static uint32_t ms5611_readRawTemp() {
     uint32_t D2;
     //Convert temp
     ms5611_write(tempAddr);
@@ -149,8 +146,7 @@ static uint32_t ms5611_readRawTemp()
     return D2;
 }
 
-static uint32_t ms5611_readRawPressure()
-{
+static uint32_t ms5611_readRawPressure() {
     uint32_t D1;
     //Convert pressure
     ms5611_write(pressAddr);
@@ -162,15 +158,12 @@ static uint32_t ms5611_readRawPressure()
     return D1;
 }
 
-void Barometer_init()
-{
+void Barometer_init() {
     ms5611_init();
 }
 
-void Barometer_setOSR(OSR osr)
-{
-    switch(osr)
-    {
+void Barometer_setOSR(OSR osr) {
+    switch (osr) {
         default:
         case OSR_256:
             pressAddr = PRESSURE_OSR_256;
@@ -200,35 +193,28 @@ void Barometer_setOSR(OSR osr)
     }
 }
 
-int32_t Barometer_getTemp(bool calculate)
-{
-    if (calculate)
-    {
+int32_t Barometer_getTemp(bool calculate) {
+    if (calculate) {
         Barometer_calculate();
     }
     return temperature;
 }
 
-int32_t Barometer_getPressure(bool calculate)
-{
-    if (calculate)
-    {
+int32_t Barometer_getPressure(bool calculate) {
+    if (calculate) {
         Barometer_calculate();
     }
     return pressure;
 }
 
-float Barometer_getAltitude(bool calculate)
-{
-    if (calculate)
-    {
+float Barometer_getAltitude(bool calculate) {
+    if (calculate) {
         Barometer_calculate();
     }
     return altitude;
 }
 
-void Barometer_calculate()
-{
+void Barometer_calculate() {
     int32_t dT;
     int64_t TEMP, OFF, SENS, P;
     uint32_t D1, D2;
@@ -237,27 +223,27 @@ void Barometer_calculate()
     D1 = ms5611_readRawPressure();
     D2 = ms5611_readRawTemp();
 
-    dT = D2-((long)prom[4]*256);
-    TEMP = 2000 + ((int64_t)dT * prom[5])/8388608;
-    OFF = (int64_t)prom[1] * 65536 + ((int64_t)prom[3] * dT ) / 128;
-    SENS = (int64_t)prom[0] * 32768 + ((int64_t)prom[2] * dT) / 256;
+    dT = D2 - ((long) prom[4] * 256);
+    TEMP = 2000 + ((int64_t) dT * prom[5]) / 8388608;
+    OFF = (int64_t) prom[1] * 65536 + ((int64_t) prom[3] * dT) / 128;
+    SENS = (int64_t) prom[0] * 32768 + ((int64_t) prom[2] * dT) / 256;
 
-    if (TEMP < 2000){   // second order temperature compensation
-        int64_t T2 = (((int64_t)dT)*dT) >> 31;
-        int64_t Aux_64 = (TEMP-2000)*(TEMP-2000);
-        int64_t OFF2 = (5*Aux_64)>>1;
-        int64_t SENS2 = (5*Aux_64)>>2;
+    if (TEMP < 2000) {   // second order temperature compensation
+        int64_t T2 = (((int64_t) dT) * dT) >> 31;
+        int64_t Aux_64 = (TEMP - 2000) * (TEMP - 2000);
+        int64_t OFF2 = (5 * Aux_64) >> 1;
+        int64_t SENS2 = (5 * Aux_64) >> 2;
         TEMP = TEMP - T2;
         OFF = OFF - OFF2;
         SENS = SENS - SENS2;
     }
 
-    P = (D1*SENS/2097152 - OFF)/32768;
+    P = (D1 * SENS / 2097152 - OFF) / 32768;
     temperature = TEMP;
     pressure = P;
 
-    press = (float)pressure;
-    r= press/101325.0;
-    c = 1.0/5.255;
-    altitude = (1 - pow(r,c))*44330.77;
+    press = (float) pressure;
+    r = press / 101325.0;
+    c = 1.0 / 5.255;
+    altitude = (1 - pow(r, c)) * 44330.77;
 }
