86b3823cd3d4152bde43bf1dd682e1a768ea7cd4
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2022-03-08 09:57:56 -0500

    added docstring, added n-saftey

--- source/modules/max7c0/ublox7rx.c
+++ source/modules/max7c0/ublox7rx.c
@@ -1,13 +1,36 @@
 #include "ublox7rx.h"
 
+/*
+ * @brief	Parse GGA sentence into gps_packet_t
+ *
+ * @param	sentence	pointer to beginning of GGA sentence
+ * @param	dest		pointer to GPS packet
+ * @retval	0 for success, -1 for failure (as determined by sscanf return value)
+ */
 int32_t parse_gga(char* sentence, gps_packet_t* dest, size_t n)
 {
+	// adds null at position n if no null before n for sscanf saftey
+	int ret = 1;
+
+	// this part is untested
+	for (int i = 0; i < n; i++) {
+		if sentence[i] == '\0' {
+			ret = 0;
+			break;
+		}
+	}
+	if (ret) {
+		sentence[n] = '\0';
+		ret = 0;
+	}
+	
+	// everything after this is tested
 	// initalizing buffers
 	char north, east;
 	int hours, minutes, lat_deg, lon_deg;
 	float lat_min, lon_min;
 
-	int ret = sscanf(
+	ret = sscanf(
 		sentence, 
 		"$%*2cGGA,%2d%2d%f,%2d%f,%c,%3d%f,%c,%1d,%2d,%*f,%f,%*f,%*f,%*f,%*2c\r\n",
 		&hours, &minutes, &(dest->time), // parts of time
